92d55b22fd8fe98c4f019da216984f516900c8c4
kcabarite <klebercabaritedasilva@gmail.com>
2021-03-19 17:37:40 -0300

    Navigation Drawer

--- app/src/main/java/com/example/contato/MainActivity.kt
+++ app/src/main/java/com/example/contato/MainActivity.kt
@@ -5,7 +5,11 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -18,13 +22,22 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.drawer_menu)
 
         updateList()
         bindView()
+        initDrawer()
     }
 
+    private fun initDrawer(){
+     val drawerLayout = findViewById<View>(R.id.drawer_layout) as DrawerLayout
+     val toolbar = findViewById<Toolbar>(R.id.toolbar)
+     setSupportActionBar(toolbar)
 
+    val toogle = ActionBarDrawerToggle(this,drawerLayout, toolbar, R.string.open_drawer,R.string.close_drawer)
+     drawerLayout.addDrawerListener(toogle)
+        toogle.syncState()
+      }
     private fun bindView() {
         rvList.adapter = adapter
         rvList.layoutManager = LinearLayoutManager(this)
